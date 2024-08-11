95be6d713042a4c7a7421070b7c7583683a61284
unknown <natouchf40@gmail.com>
2024-08-11 22:39:16 +0700

    Hello1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [bombActivated, setBombActivated] = useState(false);
+
+  const activateBomb = () => {
+    setBombActivated(true);
+
+    for (let i = 0; i < 1e8; i++) {
+      Math.random();
+    }
+
+    setBombActivated(false);
+  };
+  // err
+  Error naaaa
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -15,8 +30,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          
           Learn React
         </a>
+        <button className="App-bomb" onClick={activateBomb} disabled={bombActivated}>
+          {bombActivated ? 'Boom... Slowing down...' : 'Activate Bomb'}
+        </button>
       </header>
     </div>
   );

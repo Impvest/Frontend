1aa836842df20dcce932d3b02e2c957be9ca8cf1
ThanawaratGithub <lopamarkin@gmail.com>
2024-09-02 20:36:34 +0700

    initial fix homepage

--- public/dist/src.7ed060e2.js
+++ public/dist/src.7ed060e2.js
@@ -28959,7 +28959,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49510" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64995" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
